437bfdc9304063fb045af338741f0e1a32b84bde
asmaatarek <asmaatarek06@gmail.com>
2024-10-20 21:53:01 +0300

    test_app

--- app/test_app.py
+++ app/test_app.py
@@ -1,3 +1,19 @@
-def test_hello():
-        assert True
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_homepage(client):
+    """Test that the homepage loads successfully."""
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"DEPI DevOps Project" in response.data
+
+def test_static_file(client):
+    """Test that the static file (image) is served correctly."""
+    response = client.get('/static/docker2.jpg')
+    assert response.status_code == 200
 
